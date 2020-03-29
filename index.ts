6ae995cc9b596fbc03c636a58e56b23158facfa3
UladzimirKudzelka <vladimirkudelko1998@gmail.com>
2020-03-29 13:10:28 +0300

    change socket.io port

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import * as http from 'http';
 import * as socket from 'socket.io';
-import * as TelegramBot from 'node-telegram-bot-api';
 
 import app from './server';
 
 const port = process.env.PORT || 3001;
-const server = new http.Server(app);
-const telegramBot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+const server = http.createServer(app);
 
 export const io = socket(server, {
   serveClient: false,
@@ -15,31 +13,8 @@ export const io = socket(server, {
   cookie: false
 });
 
-telegramBot.on('message', (message) => {
-  const { chat: { id } } = message;
-
-  telegramBot.sendMessage(id, 'Choose something: ', {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{ text: '✅ Кнопка 1', callback_data: 'data 1' }],
-        [{ text: '✅ Кнопка 2', callback_data: 'data 2' }],
-        [{ text: '✅ Кнопка 3', callback_data: 'data 3' }]
-      ]
-    }) as any
-  });
-});
-
-telegramBot.on('callback_query', (message) => {});
-
-telegramBot.onText(/\/help/, (message, [source, match]) => {
-  const { chat: { id } } = message;
-
-  telegramBot.sendMessage(id, match);
-});
-
 io.on('connection', (socketIO) => {
   console.log('New Socket Connection');
 });
 
-app.listen(port, () => console.log(`Server is starting on port - ${port}`));
-server.listen(8000);
+server.listen(port, () => console.log(`Server is starting on port - ${port}`));
